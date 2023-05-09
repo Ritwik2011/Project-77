833a715245edc98ff600ad298045d3d0ecf8ef9d
Ritwik2011 <110495025+Ritwik2011@users.noreply.github.com>
2023-05-09 16:15:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var a = 10;
+var b = 5;
+console.log("first_name = "+ first_name);
+console.log("last_name = "+ last_name);
+console.log("10x5 = " + (a*b));
